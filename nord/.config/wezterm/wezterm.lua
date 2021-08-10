c5630207eade3d753f97d1cdbd5092e392002419
ayamir <lgt986452565@gmail.com>
2021-08-10 12:06:14 +0800

    [switch] update wezterm's settings.

--- nord/.config/wezterm/wezterm.lua
+++ nord/.config/wezterm/wezterm.lua
@@ -1,11 +1,19 @@
 local wezterm = require 'wezterm';
 return {
-    default_prog = {"/usr/bin/zsh", "-l"},
-    font = wezterm.font("JetBrainsMono Nerd Font", {weight = "Regular"}),
+    default_prog = {"/usr/bin/fish", "-l"},
+    font = wezterm.font_with_fallback({
+        "JetBrainsMono Nerd Font", "Sarasa Mono SC Nerd", "FiraCode Nerd Font"
+    }),
     front_end = "OpenGL",
     font_size = 12,
-    color_scheme = "OneHalfLight",
+    color_scheme = "nord",
     enable_tab_bar = false,
-    text_background_opacity = 0.8,
-    window_background_opacity = 0.8
+    text_background_opacity = 0.90,
+    window_background_image = "/home/ayamir/Pictures/wezterm/nord.jpg",
+
+    window_background_image_hsb = {
+        brightness = 0.9,
+        hue = 1.0,
+        saturation = 1.0
+    }
 }
