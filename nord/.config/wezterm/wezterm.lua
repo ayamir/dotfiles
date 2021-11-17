3127810f86a23ba95d80fd39627bce5350a584af
ayamir <lgt986452565@gmail.com>
2021-11-17 10:01:07 +0800

    [wezterm] update light config.

--- nord/.config/wezterm/wezterm.lua
+++ nord/.config/wezterm/wezterm.lua
@@ -1,18 +1,21 @@
-local wezterm = require 'wezterm';
+local wezterm = require "wezterm"
 return {
     default_prog = {"/usr/bin/fish", "-l"},
-    font = wezterm.font_with_fallback({
-        "JetBrainsMono Nerd Font", "Sarasa Mono SC Nerd", "FiraCode Nerd Font"
-    }),
+    font = wezterm.font_with_fallback(
+        {
+            "JetBrainsMono Nerd Font",
+            "Sarasa Mono SC Nerd",
+            "FiraCode Nerd Font"
+        }
+    ),
     front_end = "OpenGL",
     font_size = 12,
-    color_scheme = "nord",
+    color_scheme = "OneHalfLight",
     enable_tab_bar = false,
-    text_background_opacity = 0.90,
-    window_background_image = "/home/ayamir/Pictures/wezterm/nord.jpg",
-
+    text_background_opacity = 0.1,
+    window_background_image = "/home/ayamir/Pictures/wezterm/OneHalfLight.jpg",
     window_background_image_hsb = {
-        brightness = 0.9,
+        brightness = 1.0,
         hue = 1.0,
         saturation = 1.0
     }
