904e0754a20abd9566717e492393ee7cd7f05e32
ayamir <lgt986452565@gmail.com>
2022-05-13 12:16:23 +0800

    [polybar] remove electronNCM.

--- catppuccin/.config/polybar/script/get_spotify_status.sh
+++ catppuccin/.config/polybar/script/get_spotify_status.sh
@@ -9,7 +9,7 @@ PARENT_BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
 # Examples: spotify, vlc, chrome, mpv and others.
 # Use `playerctld` to always detect the latest player.
 # See more here: https://github.com/altdesktop/playerctl/#selecting-players-to-control
-PLAYER="ElectronNCM"
+PLAYER="playerctld"
 
 # Format of the information displayed
 # Eg. {{ artist }} - {{ album }} - {{ title }}
